54dc0bbb5a58b02b50a1b84526f1d23ae95f7577
hasan <blackbee08@gmail.com>
2021-10-06 17:57:31 +0700

    Implementing the Comment Service

--- internal/comment/comment.go
+++ internal/comment/comment.go
@@ -1,6 +1,8 @@
 package comment
 
-import "github.com/eatigo/gorm"
+import (
+	"github.com/eatigo/gorm"
+)
 
 //Service - the struct for the comment service
 type Service struct {
@@ -18,7 +20,7 @@ type Comment struct {
 //CommentService - the interface for comment service
 type CommentService interface {
 	GetComment(ID uint) (Comment, error)
-	GetCommentBySlug(slug string) (Comment, error)
+	GetCommentsBySlug(slug string) ([]Comment, error)
 	PostComment(comment Comment) (Comment, error)
 	UpdateComment(ID uint, newComent Comment) (Comment, error)
 	DeleteComment(ID uint) error
@@ -30,4 +32,61 @@ func NewService(db *gorm.DB) *Service {
 	return &Service{
 		DB: db,
 	}
+}
+
+//GetComment - retrieves comment by comment ID from the database
+func (s *Service) GetComment(ID uint) (Comment, error) {
+	var comment Comment
+	if result := s.DB.First(&comment, ID); result.Error != nil {
+		return Comment{}, result.Error
+	}
+	return comment, nil
+}
+
+// GetCommentsBySlug - retrieves all comments by slug (path - /article/name/)
+func (s *Service) GetCommentsBySlug(slug string) ([]Comment, error) {
+	var comments []Comment
+	if result := s.DB.Find(&comments).Where("slug = ?", slug); result.Error != nil {
+		return []Comment{}, result.Error
+	}
+	return comments, nil
+}
+
+// PostComment - adds a new comment to the database
+func (s *Service) PostComment(comment Comment) (Comment, error) {
+	if result := s.DB.Save(&comment); result.Error != nil {
+		return Comment{}, result.Error
+	}
+	return comment, nil
+}
+
+// UpdateComment - updates a comment by ID with new comment info
+func (s *Service) UpdateComment(ID uint, newComment Comment) (Comment, error) {
+	comment, err := s.GetComment(ID)
+	if err != nil {
+		return Comment{}, err
+	}
+
+	if result := s.DB.Model(&comment).Updates(newComment); result.Error != nil {
+		return Comment{}, result.Error
+	}
+
+	return comment, nil
+}
+
+// DeleteComment - deletes a comment from the database by ID
+func (s *Service) DeleteComment(ID uint) error {
+	if result := s.DB.Delete(&Comment{}, ID); result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+//GetAllComments - retrieves all the comments from the database
+func (s *Service) GetAllComments() ([]Comment, error) {
+	var comments []Comment
+	if result := s.DB.Find(&comments); result.Error != nil {
+		return []Comment{}, result.Error
+	}
+	return comments, nil
 }
