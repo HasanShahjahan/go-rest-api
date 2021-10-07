14560a0bcf6e3e4067c8cf4006a22eb61965d5ce
hasan <blackbee08@gmail.com>
2021-10-07 13:31:27 +0700

    Implementing The Comment REST API Endpoints

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/HasanShahjahan/go-rest-api/internal/comment"
 	"github.com/HasanShahjahan/go-rest-api/internal/database"
 	transportHTTP "github.com/HasanShahjahan/go-rest-api/internal/transport/http"
 )
@@ -18,12 +19,13 @@ func (a *App) Run() error {
 	fmt.Println("Setting up APP")
 
 	var err error
-	_, err = database.NewDatabase()
+	db, err := database.NewDatabase()
 	if err != nil {
 		return err
 	}
 
-	handler := transportHTTP.NewHandler()
+	commentService := comment.NewService(db)
+	handler := transportHTTP.NewHandler(commentService)
 	handler.SetupRoutes()
 
 	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
