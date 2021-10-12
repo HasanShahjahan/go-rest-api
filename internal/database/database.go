8a49a58ccea26cfa400ee843257a16fdcd7d41e5
hasan <blackbee08@gmail.com>
2021-10-12 16:30:34 +0700

    Defining K8s Deployment

--- internal/database/database.go
+++ internal/database/database.go
@@ -17,7 +17,7 @@ func NewDatabase() (*gorm.DB, error) {
 	dbTable := os.Getenv("DB_TABLE")
 	dbPort := os.Getenv("DB_PORT")
 
-	connectionString := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", dbHost, dbPort, dbUsername, dbTable, dbPassword)
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=require", dbHost, dbPort, dbUsername, dbTable, dbPassword)
 	fmt.Println(connectionString)
 
 	db, err := gorm.Open("postgres", connectionString)
